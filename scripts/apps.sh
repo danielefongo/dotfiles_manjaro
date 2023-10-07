ab2d0f2c1d5855af25462d20789604f6c4b6e421
danielefongo <danielefongo@gmail.com>
2023-10-07 19:08:31 +0200

    feat: add zsh and some utils

---
+++ scripts/apps.sh
@@ -0,0 +1,10 @@
+# essentials
+yay -S make --noconfirm
+
+# terminal
+yay -S zsh --noconfirm
+yay -s $(which zsh) $(whoami)
+yay -S autojump-rs --noconfirm
+yay -S fzf --noconfirm
+yay -S ripgrep --noconfirm
+curl https://rtx.pub/install.sh | sh
