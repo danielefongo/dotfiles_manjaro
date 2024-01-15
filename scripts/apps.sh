a2a79014ff21aa1e76c2cbde4d51b6d62ce61ddb
danielefongo <danielefongo@gmail.com>
2024-01-15 16:33:21 +0100

    feat: migrate from rtx to mise

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -8,13 +8,14 @@ yay -s $(which zsh) $(whoami)
 yay -S autojump-rs --noconfirm
 yay -S fzf --noconfirm
 yay -S ripgrep --noconfirm
-curl https://rtx.pub/install.sh | sh
+
+curl https://mise.jdx.dev/install.sh | sh
+mise install
 yay -S neovim --noconfirm
 yay -S tmux --noconfirm
 yay -S git --noconfirm
 yay -S tig --noconfirm
 yay -S hub --noconfirm
-yay -S delta --noconfirm
 
 # utils
 yay -S alacritty --noconfirm
