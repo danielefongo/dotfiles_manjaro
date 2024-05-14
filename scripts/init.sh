3a2fb0e01fe8fdd1f13de321ae6f84221c7acaea
danielefongo <danielefongo@gmail.com>
2024-05-14 12:07:16 +0200

    fix: dots installation

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,5 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed git base-devel --noconfirm
+sudo pacman -S --needed git base-devel npm --noconfirm
 sudo pacman -S yay --noconfirm
+
+./dots/scripts/dots_setup
